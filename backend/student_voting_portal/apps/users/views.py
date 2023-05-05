caab23ce3c5e1160f4fd43a60a8cce2277e1fbdd
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-05 00:15:32 -0700

    style: format

--- backend/student_voting_portal/apps/users/views.py
+++ backend/student_voting_portal/apps/users/views.py
@@ -1,15 +1,7 @@
-from rest_framework.generics import GenericAPIView, CreateAPIView
-from rest_framework.request import Request
-from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
 
 from student_voting_portal.apps.users.serializers import UserSerializer
 
 
 class UserView(CreateAPIView):
     serializer_class = UserSerializer
-
-    # def post(self, request: Request) -> Response:
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     # serializer.save()
-    #     return Response(serializer.data)
