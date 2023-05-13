9ce04428086549c12dcdc22520d97436be54d93c
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-13 16:39:01 -0700

    style: rm unnecessary code

--- backend/student_voting_portal/apps/elections/views.py
+++ backend/student_voting_portal/apps/elections/views.py
@@ -5,29 +5,28 @@ from rest_framework.viewsets import ModelViewSet
 
 from elections.models import Election, Position, Vote, Candidate
 from elections.serializers import ElectionSerializer, PositionSerializer, CandidateSerializer, VoteSerializer
-from student_voting_portal.utils.views import BaseViewSet
 from student_voting_portal.utils.permissions import IsOwnerOrAdmin, VotePermission, Get, IsSameUniversity
 
 
-class ElectionView(BaseViewSet, ModelViewSet):
+class ElectionView(ModelViewSet):
     serializer_class = ElectionSerializer
     queryset = Election.objects.all()
     permission_classes = [Get | (IsAdminUser & IsSameUniversity)]
 
 
-class PositionView(BaseViewSet, ModelViewSet):
+class PositionView(ModelViewSet):
     serializer_class = PositionSerializer
     queryset = Position.objects.all()
     permission_classes = [Get | (IsAdminUser & IsSameUniversity)]
 
 
-class CandidateView(BaseViewSet, ModelViewSet):
+class CandidateView(ModelViewSet):
     serializer_class = CandidateSerializer
     queryset = Candidate.objects.all()
     permission_classes = [Get | (IsOwnerOrAdmin & IsSameUniversity)]
 
 
-class VoteView(BaseViewSet, CreateAPIView):
+class VoteView(CreateAPIView):
     serializer_class = VoteSerializer
     queryset = Vote.objects.all()
     permission_classes = [VotePermission]
