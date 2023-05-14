1439fe6359ef7405fd89d5808d1e04632717936e
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-13 23:17:14 -0700

    feat: docker compose

---
+++ mysql/docker-entrypoint-initdb.d/create-user-and-db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create the initial database and user
+mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%';"
+mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON test_$MYSQL_DATABASE.* TO $MYSQL_USER@'%';"
