efff77508e6e57303ecb9f295f35c06813b56047
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-25 18:06:07 -0700

    feat: election results and types

--- frontend/src/Interfaces/Election.ts
+++ frontend/src/Interfaces/Election.ts
@@ -1,39 +1,4 @@
-export default interface User {
-  id: number;
-  email: string;
-  dob: string;
-  staff: boolean;
-  superuser: boolean;
-}
-
-export interface Token {
-  access: string;
-  refresh: string;
-}
-
-export interface University {
-  id: number;
-  name: string;
-}
-
-export interface LoginParams {
-  email: string;
-  password: string;
-}
-
-export interface LoginResponse extends User {
-  token: Token;
-  university: University;
-}
-
-export interface createUser{
-  email: string,
-  password: string,
-  passwordConfirm: string,
-  universityId: number,
-  dob: Date,
-  admin: Boolean
-}
+import { University } from "./User";
 
 export interface Vote {
   electionId: number;
@@ -75,13 +40,6 @@ export interface Position {
   maxVotesPerCandidate: number;
 }
 
-export interface ElectionData {
-  code: number;
-  success: boolean;
-  msg: string;
-  data: ElectionDetail[];
-}
-
 export interface ElectionDetail {
   id: number;
   positions: PositionDetail[];
@@ -111,9 +69,7 @@ export interface CandidateDetail {
   candidateName: string;
   candidateDesc: string;
   photoUrl: string;
-}
-
-export interface University {
-  id: number;
-  name: string;
+  // election result
+  winner?: boolean;
+  votePercentage?: number;
 }
---
+++ frontend/src/Interfaces/User.ts
@@ -0,0 +1,42 @@
+export default interface User {
+  id: number;
+  email: string;
+  dob: string;
+  staff: boolean;
+  superuser: boolean;
+}
+
+export interface Token {
+  access: string;
+  refresh: string;
+}
+
+export interface University {
+  id: number;
+  name: string;
+}
+
+export interface LoginParams {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse extends User {
+  token: Token;
+  university: University;
+}
+
+export interface CreateUserParams {
+  email: string,
+  password: string,
+  passwordConfirm: string,
+  universityId: number,
+  dob: Date,
+  admin: Boolean
+}
+
+
+export interface University {
+  id: number;
+  name: string;
+}
