fe1e29d0f0cbf6d58254b87ba5d40ce88bdacea4
yiyix11 <xiangyiyi11@gmail.com>
2023-05-28 17:51:13 -0700

    Update manage user function with API

--- frontend/src/pages/admin/ManageUsers.tsx
+++ frontend/src/pages/admin/ManageUsers.tsx
@@ -1,71 +1,117 @@
-import React, { useState } from "react";
-import { Container, Button, ListGroup, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Button, ListGroup, Row, Table, Alert } from "react-bootstrap";
 import DeleteModal from "../../component/admin/DeleteModal";
+import myApi from "../../service/MyApi";
+import User, { LoginResponse } from "../../Interfaces/User";
+import { CurrentUser, currentUser } from "../../model/User.model";
 
-function ManageUsers({ navigate }) {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearch = () => {};
-
-  const [items, setItems] = useState(["Item 1", "Item 2", "Item 3"]);
-
+function ManageUsers() {
+  // const [searchTerm, setSearchTerm] = useState("");
+  const [managedUsers, setManagedUsers] = useState<CurrentUser[]>([]);
+  const [userToDelete, setUserToDelete] = useState<CurrentUser | null>(null);
   const [deleteModalShow, setDeleteModalShow] = useState(false);
+  const [alertShow, setAlertShow] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
+  const [variant, setVariant] = useState('');
 
-  const handleDelete = (index) => {
-    const updatedItems = [...items];
-    updatedItems.splice(index, 1);
-    setItems(updatedItems);
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const fetchUser = async () => {
+    const userResult = await myApi.getUsers();
+    if (userResult.success) {
+      const filtered = (userResult.data as CurrentUser[]).filter((user) => {
+        const u = new CurrentUser();
+        u.setUser(user);
+        return (u.university.id === currentUser.university.id) && !u.isAdmin;
+      });
+      console.log("[ManageUser] data:", filtered);
+      setManagedUsers(filtered);
+    } else {
+      // Handle error
+      console.log("[ManageUser] getUsers failed: " + userResult.msg);
+    }
+  }
+
+  const deleteUser = async (userToDelete) => {
+
+
+    const result = await myApi.deleteUser(userToDelete.id);
+
+    console.log("[ManageUsers] delete user result", result)
+    if (result) {
+      setVariant("danger")
+      setAlertMsg("Failed to delete. " + result.msg)
+      setAlertShow(true)
+    } else {
+      console.log("[Manage User]: deleteUser", userToDelete);
+      const updatedUsers = managedUsers.filter((user) => user.id !== userToDelete.id);
+      setManagedUsers(updatedUsers);
+      setVariant("success")
+      setAlertMsg("Delete success!")
+      setAlertShow(true)
+    }
+    setUserToDelete(null);
+    setDeleteModalShow(false);
   };
+
+  const alert = () => {
+    if (alertShow) {
+      return (
+        <Alert variant={variant} onClose={() => setAlertShow(true)} dismissible>
+          {alertMsg}
+        </Alert>);
+    }
+  }
 
   return (
+
     <div
       className="container justify-content-center"
-      style={{ padding: "20px" }}
-    >
-      <input
-        type="text"
-        placeholder="Search Users..."
-        value={searchTerm}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleSearch}>Search</button>
+      style={{ padding: "20px" }}>
 
-      <div>
-        <Container>
-          <ListGroup>
-            {items.map((item, index) => (
-              <ListGroup.Item className="d-flex" key={index}>
-                <div
-                  className="ms-2 me-auto"
-                  style={{ display: "flex", alignItems: "center" }}
-                >
-                  {item}
-                </div>
-                <Button
-                  variant="outline-danger"
-                  onClick={() => setDeleteModalShow(true)}
-                  //   onClick={() => handleDelete(1)}
-                >
-                  Delete
-                </Button>
+      {alert()}
 
-                {/* <DeleteModal
-                  show={deleteModalShow}
-                  onHide={() => setDeleteModalShow(false)}
-                /> */}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Container>
-        <DeleteModal
-          show={deleteModalShow}
-          onHide={() => setDeleteModalShow(false)}
-        />
-      </div>
+      <Container>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th colSpan={5}>Username</th>
+
+              <th>Operater</th>
+            </tr>
+          </thead>
+          {managedUsers.map((user, index) => (
+            <tbody className="striped">
+              <tr key={index}>
+                <td className="align-middle">{index + 1}</td>
+                <td className="align-middle" colSpan={5}>{user.email}</td>
+                <td>
+                  <Button
+                    variant="outline-danger"
+                    onClick={() => {
+                      setUserToDelete(user);
+                      setDeleteModalShow(true);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </Table>
+      </Container>
+      {deleteModalShow ? (<DeleteModal
+        userToDelete={userToDelete}
+        // deleteItem={deleteItem}
+        deleteUser={deleteUser}
+        closeModal={() => setDeleteModalShow(false)}
+      />) : null}
+
     </div>
   );
 }
