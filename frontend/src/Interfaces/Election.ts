1b29940fc6c941a4b07bb8a2fab686bf5b2e74cb
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-29 15:43:37 -0700

    feat: frontend PositionDetail `totalVoteCount`

--- frontend/src/Interfaces/Election.ts
+++ frontend/src/Interfaces/Election.ts
@@ -73,6 +73,7 @@ export interface PositionDetail {
   positionDesc: string;
   maxVotesTotal: number;
   maxVotesPerCandidate: number;
+  totalVoteCount: number;
   candidates: CandidateDetail[];
 }
 
